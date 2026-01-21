fabca1b65c55646af442214192b541a6a5f2c98a
Sai Sabure Sonali R <saisaburesonali@gmail.com>
2026-01-21 21:21:09 +0530

    Create AboutActivity.java

---
+++ AboutActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+    Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        button3 = findViewById(R.id.button3);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }}
